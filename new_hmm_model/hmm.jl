02e84aa91ac443b0d29cef228afd6cb0a41748a7
Ari Kahn <ari.e.kahn@gmail.com>
2022-04-18 16:57:23 -0400

    Include norm code for <1.7

--- new_hmm_model/hmm.jl
+++ new_hmm_model/hmm.jl
@@ -9,6 +9,31 @@ using Statistics
 using StatsFuns
 using Combinatorics
 
+# Including these to fix an issue with Julia 1.6
+@static if (VERSION <= v"1.6")
+    function normalize!(a::AbstractArray, p::Real=2)
+        nrm = norm(a, p)
+        __normalize!(a, nrm)
+    end
+
+    @inline function __normalize!(a::AbstractArray, nrm::Real)
+        # The largest positive floating point number whose inverse is less than infinity
+        δ = inv(prevfloat(typemax(nrm)))
+
+        if nrm ≥ δ # Safe to multiply with inverse
+            invnrm = inv(nrm)
+            rmul!(a, invnrm)
+
+        else # scale elements to avoid overflow
+            εδ = eps(one(nrm))/δ
+            rmul!(a, εδ)
+            rmul!(a, inv(nrm*εδ))
+        end
+
+        a
+    end
+end
+
 animals = [
     "senor",
     "chimi",
