727e1d0047bd429b611da1dfa543df3bf6e98207
Ari Kahn <ari.e.kahn@gmail.com>
2022-05-11 18:08:30 -0400

    cluster contingency comparison

---
+++ cluster/hmm_contingency.jl
@@ -0,0 +1,25 @@
+using StatsPlots
+using FileIO
+using EM
+include("../code/hmm.jl")
+include("../code/em_scripts.jl")
+include("../code/util.jl")
+
+# number of contingencies
+i = parse(Int, ARGS[1])
+(fn_ind, animal_ind) = fldmod(i - 1, length(animals)) .+ 1
+animal = animals[animal_ind]
+data = load_animal(animal, "..")
+println(animal)
+println(fn_ind)
+if fn_ind == 1
+	results = run_hmm_leaf_stay_turn_leafspatial(data; ϕ=get_contingencies_base());
+	save("../results/hmm_contingency/contingencies_base_$(animal).jld2", "contingencies_base_$(animal)", results)
+elseif fn_ind == 2
+	results = run_hmm_leaf_stay_turn_leafspatial(data; ϕ=get_contingencies_observed());
+	save("../results/hmm_contingency/contingencies_observed_$(animal).jld2", "contingencies_observed_$(animal)", results)
+else
+	n = fn_ind - 2
+	results = run_hmm_leaf_stay_turn_leafspatial(data; ϕ=get_contingencies(;n=n));
+	save("../results/hmm_contingency/contingencies_$(n)_$(animal).jld2", "contingencies_$(n)_$(animal)", results)
+end
---
+++ cluster/run_hmm_contingency.sh
@@ -0,0 +1,1 @@
+sbatch --array=1-50 ./hmm_contingency.sh
