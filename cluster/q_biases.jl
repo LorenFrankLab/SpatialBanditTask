5b087e9ada463a55497fc11dde7c692ca9638ade
Ari Kahn <ari.e.kahn@gmail.com>
2022-05-23 20:47:10 -0400

    QInitial convenience functions and cluster batch

--- cluster/q_biases.jl
+++ cluster/q_biases.jl
@@ -26,6 +26,27 @@ fns = [
 ("q_leaf_stay_turn_leafturn_γ2", run_q_leaf_stay_turn_leafturn_γ2),
 ("q_leaf_stay_spatial_leafspatial_γ2", run_q_leaf_stay_spatial_leafspatial_γ2),
 ("q_leaf_stay_spatial_leafturn_γ2", run_q_leaf_stay_spatial_leafturn_γ2),
+
+("q_leaf_initialQ", run_q_leaf_initialQ),
+("q_leaf_initialQ_γ2", run_q_leaf_initialQ_γ2),
+("q_leaf_initialQ_retainbelief", run_q_leaf_initialQ_retainbelief),
+("q_leaf_initialQ_stay", run_q_leaf_initialQ_stay),
+("q_leaf_initialQ_stay_γ2", run_q_leaf_initialQ_stay_γ2),
+("q_leaf_initialQ_stay_retainbelief", run_q_leaf_initialQ_stay_retainbelief),
+
+("q_leaf_initialQ_stay_turn", run_q_leaf_initialQ_stay_turn),
+("q_leaf_initialQ_stay_spatial", run_q_leaf_initialQ_stay_spatial),
+("q_leaf_initialQ_stay_turn_leafspatial", run_q_leaf_initialQ_stay_turn_leafspatial),
+("q_leaf_initialQ_stay_turn_leafturn", run_q_leaf_initialQ_stay_turn_leafturn),
+("q_leaf_initialQ_stay_spatial_leafspatial", run_q_leaf_initialQ_stay_spatial_leafspatial),
+("q_leaf_initialQ_stay_spatial_leafturn", run_q_leaf_initialQ_stay_spatial_leafturn),
+
+("q_leaf_initialQ_stay_turn_γ2", run_q_leaf_initialQ_stay_turn_γ2),
+("q_leaf_initialQ_stay_spatial_γ2", run_q_leaf_initialQ_stay_spatial_γ2),
+("q_leaf_initialQ_stay_turn_leafspatial_γ2", run_q_leaf_initialQ_stay_turn_leafspatial_γ2),
+("q_leaf_initialQ_stay_turn_leafturn_γ2", run_q_leaf_initialQ_stay_turn_leafturn_γ2),
+("q_leaf_initialQ_stay_spatial_leafspatial_γ2", run_q_leaf_initialQ_stay_spatial_leafspatial_γ2),
+("q_leaf_initialQ_stay_spatial_leafturn_γ2", run_q_leaf_initialQ_stay_spatial_leafturn_γ2),
 ]
 i = parse(Int, ARGS[1])
 (fn_ind, animal_ind) = fldmod1(i, length(animals))
