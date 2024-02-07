7cf978137bbfcf3641c8f67317ffb508cba539ef
Ari Kahn <arik@princeton.edu>
2024-02-07 08:31:00 -0500

    reduce number of conds

--- cluster/run_hmm_partial_independence_biases_local.sh
+++ cluster/run_hmm_partial_independence_biases_local.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 mkdir -p ../results/hmm_partial_independence_biases
-for i in {1..960}; do
+for i in {1..162}; do
     JULIA_NUM_THREADS=8 julia hmm_partial_independence_biases.jl $i
 done 
--- cluster/run_hmm_partial_independence_biases_slurm.sh
+++ cluster/run_hmm_partial_independence_biases_slurm.sh
@@ -1,1 +1,1 @@
-sbatch --array=1-960 ./hmm_partial_independence_biases_slurm.sh
+sbatch --array=1-162 ./hmm_partial_independence_biases_slurm.sh
