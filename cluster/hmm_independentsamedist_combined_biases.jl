e890fc8516b80d57a4a1fffe8f2aa8facec6ef4e
Ari Kahn <arik@princeton.edu>
2024-02-08 15:42:45 -0500

    samedist

---
+++ cluster/hmm_independentsamedist_combined_biases.jl
@@ -0,0 +1,116 @@
+using Logging
+using StatsPlots
+using FileIO
+using JLD2
+using CodecZlib
+using EM
+include("../code/hmm_independentsamedist.jl")
+include("../code/util.jl")
+include("../code/em_scripts.jl")
+
+function find_Q_vals_by_day(data, results; add_leaf=true, rewscaled, delay_turn_bias)
+    ndays = maximum(data.daynum)
+    liks = zeros(ndays)
+    dfs = []
+    for i in 1:ndays
+        (liks[i], df) = hmm_independentsamedist_lik(view(data, data.daynum .== i, :), results;
+        subject=i, add_leaf=add_leaf, rewscaled=rewscaled, delay_turn_bias=delay_turn_bias, record=true)
+        push!(dfs, df)
+    end
+    record_df = vcat(dfs...) # Combine all session results
+    hcat(data, record_df) # Append columns to the original data
+end
+
+fns = [
+    # ("hmm_independentsamedist_leaf_depletion", run_hmm_independentsamedist_leaf_depletion),
+    # ("hmm_independentsamedist_leaf_γ2_depletion", run_hmm_independentsamedist_leaf_γ2_depletion),
+    # ("hmm_independentsamedist_leaf_retainbelief_depletion", run_hmm_independentsamedist_leaf_retainbelief_depletion),
+    ("hmm_independentsamedist_leaf_stay_depletion", run_hmm_independentsamedist_leaf_stay_depletion),
+    # ("hmm_independentsamedist_leaf_turn_depletion", run_hmm_independentsamedist_leaf_turn_depletion),
+    # ("hmm_independentsamedist_leaf_spatial_depletion", run_hmm_independentsamedist_leaf_spatial_depletion),
+    # ("hmm_independentsamedist_leaf_leafspatial_depletion", run_hmm_independentsamedist_leaf_leafspatial_depletion),
+    # ("hmm_independentsamedist_leaf_leafturn_depletion", run_hmm_independentsamedist_leaf_leafturn_depletion),
+
+    ("hmm_independentsamedist_leaf_stay_γ2_depletion", run_hmm_independentsamedist_leaf_stay_γ2_depletion),
+    ("hmm_independentsamedist_leaf_stay_retainbelief_depletion", run_hmm_independentsamedist_leaf_stay_retainbelief_depletion),
+    ("hmm_independentsamedist_leaf_stay_turn_depletion", run_hmm_independentsamedist_leaf_stay_turn_depletion),
+    # ("hmm_independentsamedist_leaf_stay_spatial_depletion", run_hmm_independentsamedist_leaf_stay_spatial_depletion),
+    ("hmm_independentsamedist_leaf_stay_leafspatial_depletion", run_hmm_independentsamedist_leaf_stay_leafspatial_depletion),
+    ("hmm_independentsamedist_leaf_stay_leafturn_depletion", run_hmm_independentsamedist_leaf_stay_leafturn_depletion),
+    ("hmm_independentsamedist_leaf_stay_turn_leafspatial_depletion", run_hmm_independentsamedist_leaf_stay_turn_leafspatial_depletion),
+    ("hmm_independentsamedist_leaf_stay_turn_leafturn_depletion", run_hmm_independentsamedist_leaf_stay_turn_leafturn_depletion),
+    # ("hmm_independentsamedist_leaf_stay_spatial_leafspatial_depletion", run_hmm_independentsamedist_leaf_stay_spatial_leafspatial_depletion),
+    # ("hmm_independentsamedist_leaf_stay_spatial_leafturn_depletion", run_hmm_independentsamedist_leaf_stay_spatial_leafturn_depletion),
+
+    ("hmm_independentsamedist_leaf_turn_γ2_depletion", run_hmm_independentsamedist_leaf_turn_γ2_depletion),
+    # ("hmm_independentsamedist_leaf_spatial_γ2_depletion", run_hmm_independentsamedist_leaf_spatial_γ2_depletion),
+    ("hmm_independentsamedist_leaf_turn_leafspatial_γ2_depletion", run_hmm_independentsamedist_leaf_turn_leafspatial_γ2_depletion),
+    ("hmm_independentsamedist_leaf_turn_leafturn_γ2_depletion", run_hmm_independentsamedist_leaf_turn_leafturn_γ2_depletion),
+    # ("hmm_independentsamedist_leaf_spatial_leafspatial_γ2_depletion", run_hmm_independentsamedist_leaf_spatial_leafspatial_γ2_depletion),
+    # ("hmm_independentsamedist_leaf_spatial_leafturn_γ2_depletion", run_hmm_independentsamedist_leaf_spatial_leafturn_γ2_depletion),
+
+    ("hmm_independentsamedist_leaf_stay_turn_γ2_depletion", run_hmm_independentsamedist_leaf_stay_turn_γ2_depletion),
+    # ("hmm_independentsamedist_leaf_stay_spatial_γ2_depletion", run_hmm_independentsamedist_leaf_stay_spatial_γ2_depletion),
+    ("hmm_independentsamedist_leaf_stay_leafturn_γ2_depletion", run_hmm_independentsamedist_leaf_stay_leafturn_γ2_depletion),
+    ("hmm_independentsamedist_leaf_stay_leafspatial_γ2_depletion", run_hmm_independentsamedist_leaf_stay_leafspatial_γ2_depletion),
+    ("hmm_independentsamedist_leaf_stay_turn_leafspatial_γ2_depletion", run_hmm_independentsamedist_leaf_stay_turn_leafspatial_γ2_depletion),
+    ("hmm_independentsamedist_leaf_stay_turn_leafturn_γ2_depletion", run_hmm_independentsamedist_leaf_stay_turn_leafturn_γ2_depletion),
+    # ("hmm_independentsamedist_leaf_stay_spatial_leafspatial_γ2_depletion", run_hmm_independentsamedist_leaf_stay_spatial_leafspatial_γ2_depletion),
+    # ("hmm_independentsamedist_leaf_stay_spatial_leafturn_γ2_depletion", run_hmm_independentsamedist_leaf_stay_spatial_leafturn_γ2_depletion),
+
+    # ("run_hmm_independentsamedist_depletion", run_hmm_independentsamedist_depletion),
+    # ("run_hmm_independentsamedist_γ2_depletion", run_hmm_independentsamedist_γ2_depletion),
+    # ("run_hmm_independentsamedist_retainbelief_depletion", run_hmm_independentsamedist_retainbelief_depletion),
+    ("run_hmm_independentsamedist_stay_depletion", run_hmm_independentsamedist_stay_depletion),
+    # ("run_hmm_independentsamedist_turn_depletion", run_hmm_independentsamedist_turn_depletion),
+    # ("run_hmm_independentsamedist_spatial_depletion", run_hmm_independentsamedist_spatial_depletion),
+
+    # ("run_hmm_independentsamedist_stay_γ2_depletion", run_hmm_independentsamedist_stay_γ2_depletion),
+    # ("run_hmm_independentsamedist_stay_retainbelief_depletion", run_hmm_independentsamedist_stay_retainbelief_depletion),
+    ("run_hmm_independentsamedist_stay_turn_depletion", run_hmm_independentsamedist_stay_turn_depletion),
+    # ("run_hmm_independentsamedist_stay_spatial_depletion", run_hmm_independentsamedist_stay_spatial_depletion),
+
+    # ("run_hmm_independentsamedist_turn_γ2_depletion", run_hmm_independentsamedist_turn_γ2_depletion),
+    # ("run_hmm_independentsamedist_spatial_γ2_depletion", run_hmm_independentsamedist_spatial_γ2_depletion),
+    ("run_hmm_independentsamedist_stay_turn_γ2_depletion", run_hmm_independentsamedist_stay_turn_γ2_depletion),
+    # ("run_hmm_independentsamedist_stay_spatial_γ2_depletion", run_hmm_independentsamedist_stay_spatial_γ2_depletion),
+]
+base_dir = "../results/hmm_independentsamedist_combined_biases"
+i = parse(Int, ARGS[1])
+(fn_ind, animal_ind) = fldmod1(i, length(animals))
+animal = animals[animal_ind]
+data = load_animal_combined(animal, "..")
+(fn_name, fn) = fns[fn_ind]
+loocv = parse(Bool, ARGS[2])
+
+@info animal
+@info fn_name
+@info loocv
+
+function run_fn(fn_name, fn, rewscaled, delay_turn_bias, loocv)
+    # Create the base filename
+    fname = fn_name
+    fname = rewscaled ? fname * "_rewscaled" : fname
+    fname = delay_turn_bias ? fname * "_delayturnbias" : fname
+    fname *= "_$(animal)_combined"
+    @info fname
+    
+    if loocv
+        fname_loocv = fname * "_loocv"
+    
+        results = load("$(base_dir)/$(fname).jld2", fname)
+        results_loocv = fn(data; extended=true, rewscaled=rewscaled, delay_turn_bias=delay_turn_bias, loocv_data=results)
+        save("$(base_dir)_loocv/$(fname_loocv).jld2", fname_loocv, results_loocv; compress=true)
+    else
+        results = fn(data; extended=true, rewscaled=rewscaled, delay_turn_bias=delay_turn_bias)
+        save("$(base_dir)/$(fname).jld2", fname, results; compress=true)
+        write_EM_to_mat(results, "$(base_dir)/$(fname).mat"; rewscaled=rewscaled, delay_turn_bias=delay_turn_bias)
+        Q = find_Q_vals_by_day(data, results; rewscaled=rewscaled, delay_turn_bias=delay_turn_bias);
+        CSV.write("$(base_dir)/Q_vals_$(fname).csv.gz", Q; compress=true)
+    end
+end
+
+# run_fn(fn_name, fn, false, false, loocv)
+#run_fn(fn_name, fn, true, false, loocv)
+# run_fn(fn_name, fn, true, true, loocv)
+println(loocv)
---
+++ cluster/make_hmm_independentsamedist_combined_biases.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -euo pipefail
+
+N_CONDS=95
+N_CORES=8
+MAX_TIME=300
+STUDY_NAME="hmm_independentsamedist_combined_biases"
+
+SCRIPT_NAME="slurm.${STUDY_NAME}.sh"
+SLURM_RUN_NAME="run.slurm.${STUDY_NAME}.sh"
+LOCAL_RUN_NAME="run.local.${STUDY_NAME}.sh"
+
+mkdir -p ../results/${STUDY_NAME}
+
+cat << EOF > slurm_scripts/$SCRIPT_NAME
+#!/usr/bin/env bash
+
+#SBATCH -J '${STUDY_NAME}'
+#SBATCH -o ../logs/${STUDY_NAME}-%j.out
+#SBATCH -p all
+#SBATCH -t $MAX_TIME
+#SBATCH -c $N_CORES
+
+echo \$SLURM_ARRAY_TASK_ID
+
+JULIA_NUM_THREADS=$N_CORES julia ${STUDY_NAME}.jl \$SLURM_ARRAY_TASK_ID false
+EOF
+chmod u+x slurm_scripts/$SCRIPT_NAME
+
+cat << EOF > $SLURM_RUN_NAME
+#!/bin/bash
+sbatch --array=1-$N_CONDS slurm_scripts/$SCRIPT_NAME
+EOF
+chmod u+x $SLURM_RUN_NAME
+
+cat << EOF > $LOCAL_RUN_NAME
+#!/bin/bash
+
+for i in {1..$N_CONDS}; do
+    julia ${STUDY_NAME}.jl \$i false
+done 
+EOF
+chmod u+x $LOCAL_RUN_NAME
