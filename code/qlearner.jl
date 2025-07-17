5692b88fa62b0521093008f3db50e5b86dff2478
Ari Kahn <arik@princeton.edu>
2025-07-17 11:22:41 -0400

    Fix which leaf we deplete in qlearner

--- code/qlearner.jl
+++ code/qlearner.jl
@@ -187,7 +187,7 @@ function qlik(data, βgo, βstay::V, βleaf, stay_bias::W, turn_bias, spatial_bi
             end
 
             if (prevs == c1[i])
-                depletion[c2[i]] *= depletion_factor
+                depletion[c1[i], c2[i]] *= depletion_factor
             else
                 depletion .= 1
             end
